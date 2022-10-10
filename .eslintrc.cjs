c5dd36d5f36e9af5eefcb2093ba13a9afe675d84
Solomon Hawk <solomon.hawk@viget.com>
2022-10-10 17:46:13 -0400

    tooling: add import order to eslint

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -6,8 +6,44 @@ module.exports = {
     node: true,
   },
   parser: '@typescript-eslint/parser',
-  extends: [
-    'plugin:prettier/recommended',
-    'plugin:@typescript-eslint/recommended',
+  extends: ['plugin:prettier/recommended'],
+  overrides: [
+    {
+      files: ['*.ts', '*.tsx'],
+      extends: [
+        'plugin:import/recommended',
+        'plugin:import/typescript',
+        'plugin:@typescript-eslint/eslint-recommended',
+        'plugin:@typescript-eslint/recommended',
+      ],
+      rules: {
+        'import/order': [
+          'error',
+          {
+            groups: [
+              'index',
+              'sibling',
+              'parent',
+              'internal',
+              'external',
+              'builtin',
+              'object',
+              'type',
+            ],
+            alphabetize: {
+              order: 'asc',
+              caseInsensitive: true,
+            },
+          },
+        ],
+      },
+      settings: {
+        'import/ignore': ['react'],
+        'import/resolver': {
+          typescript: true,
+          node: true,
+        },
+      },
+    },
   ],
 };
