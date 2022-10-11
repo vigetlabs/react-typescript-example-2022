0a9304fe72a3cbf1cd906ad6b4a7776f2e4ecd0b
Solomon Hawk <solomon.hawk@viget.com>
2022-10-11 00:16:10 -0400

    ci: add ci, test, cov scripts

--- vite.config.ts
+++ vite.config.ts
@@ -8,7 +8,7 @@ export default defineConfig({
   test: {
     globals: true,
     environment: 'jsdom',
-    setupFiles: './src/test/setup.ts',
+    setupFiles: './src/test/setupTests.ts',
     coverage: {
       all: true,
       reporter: ['html-spa', 'text', 'clover'],
