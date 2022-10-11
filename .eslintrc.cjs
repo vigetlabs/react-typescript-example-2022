e3191d07c72d40b110def2ef94f523e0f07c3ab7
Solomon Hawk <solomon.hawk@viget.com>
2022-10-11 01:27:04 -0400

    chore: move non-ts specific eslint config out of overrides

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -6,39 +6,40 @@ module.exports = {
     node: true,
   },
   parser: '@typescript-eslint/parser',
-  extends: ['plugin:prettier/recommended'],
+  extends: ['plugin:import/recommended', 'plugin:prettier/recommended'],
+  rules: {
+    'import/order': [
+      'error',
+      {
+        groups: [
+          'index',
+          'sibling',
+          'parent',
+          'internal',
+          'external',
+          'builtin',
+          'object',
+          'type',
+        ],
+        alphabetize: {
+          order: 'asc',
+          caseInsensitive: true,
+        },
+      },
+    ],
+  },
+  settings: {
+    'import/ignore': ['react'],
+  },
   overrides: [
     {
       files: ['*.ts', '*.tsx'],
       extends: [
-        'plugin:import/recommended',
         'plugin:import/typescript',
         'plugin:@typescript-eslint/eslint-recommended',
         'plugin:@typescript-eslint/recommended',
       ],
-      rules: {
-        'import/order': [
-          'error',
-          {
-            groups: [
-              'index',
-              'sibling',
-              'parent',
-              'internal',
-              'external',
-              'builtin',
-              'object',
-              'type',
-            ],
-            alphabetize: {
-              order: 'asc',
-              caseInsensitive: true,
-            },
-          },
-        ],
-      },
       settings: {
-        'import/ignore': ['react'],
         'import/resolver': {
           typescript: true,
           node: true,
