e4da778797f7672c4733599ccc4f18187f00c42e
Solomon Hawk <solomon.hawk@viget.com>
2022-10-13 02:10:12 -0400

    fix: extract styled components from app and update test

--- src/app.tsx
+++ src/app.tsx
@@ -1,41 +1,8 @@
 import { Counter } from 'components/counter';
+import { ThemeToggleButton } from 'components/theme-toggle-button';
 import { useThemeStore } from 'features/theming/store';
 import { ConnectedThemeProvider } from 'features/theming/theme-provider';
-import styled from '@emotion/styled';
-
-const Wrapper = styled.div`
-  background: ${(props) => props.theme.colors.background};
-  padding: ${(props) => props.theme.spacing(4)};
-  min-height: 100%;
-`;
-
-const Card = styled.div`
-  background: ${(props) => props.theme.colors.surface};
-  border-radius: ${(props) => props.theme.spacing(1)};
-  padding: ${(props) => props.theme.spacing(4)};
-`;
-
-const Heading = styled.h1`
-  color: ${(props) => props.theme.colors.text};
-`;
-
-const Paragraph = styled.p`
-  color: ${(props) => props.theme.colors.text};
-`;
-
-const ThemeToggleButton = styled.button`
-  background: ${(props) => props.theme.colors.primary};
-  color: ${(props) => props.theme.colors.text};
-  border: 1px solid transparent;
-  padding: ${(props) => `${props.theme.spacing(2)} ${props.theme.spacing(3)}`};
-  border-radius: ${(props) => props.theme.spacing(1)};
-  cursor: pointer;
-
-  &:hover,
-  &:focus {
-    border: 1px solid ${(props) => props.theme.colors.secondary};
-  }
-`;
+import { Card, Heading, Paragraph, Wrapper } from 'styled';
 
 // @TODO(shawk): move this and actually do some routing
 function Router() {
@@ -52,9 +19,7 @@ function Router() {
           Theme: <strong>{themeStore.mode}</strong>
         </Paragraph>
 
-        <ThemeToggleButton onClick={() => themeStore.toggleMode()}>
-          Toggle theme
-        </ThemeToggleButton>
+        <ThemeToggleButton>Toggle theme</ThemeToggleButton>
       </Card>
     </Wrapper>
   );
