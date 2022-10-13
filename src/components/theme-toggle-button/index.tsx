c5b4ca7a20b57eff2a549c2721b9eacced5e76c0
Solomon Hawk <solomon.hawk@viget.com>
2022-10-13 01:58:10 -0400

    tests: add coverage for theme-toggle-button

---
+++ src/components/theme-toggle-button/index.tsx
@@ -0,0 +1,12 @@
+import { Button } from './styled';
+import { useThemeStore } from 'features/theming/store';
+import React from 'react';
+
+export const ThemeToggleButton = ({
+  children,
+}: {
+  children: React.ReactNode;
+}) => {
+  const themeStore = useThemeStore();
+  return <Button onClick={() => themeStore.toggleMode()}>{children}</Button>;
+};
---
+++ src/components/theme-toggle-button/theme-toggle-button.spec.tsx
@@ -0,0 +1,33 @@
+import { ThemeToggleButton } from '.';
+import { useThemeStoreImpl } from 'features/theming/store';
+import { ConnectedThemeProvider } from 'features/theming/theme-provider';
+import { render } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+
+function setup() {
+  return {
+    user: userEvent.setup(),
+    ...render(
+      <ConnectedThemeProvider>
+        <ThemeToggleButton>Toggle theme</ThemeToggleButton>
+      </ConnectedThemeProvider>,
+    ),
+  };
+}
+
+describe('<ThemeToggleButton />', () => {
+  it('toggles the theme when clicked', async () => {
+    const { user, findByRole } = setup();
+    const button = await findByRole('button');
+
+    expect(useThemeStoreImpl.getState().mode).toBe('light');
+
+    await user.click(button);
+
+    expect(useThemeStoreImpl.getState().mode).toBe('dark');
+
+    await user.click(button);
+
+    expect(useThemeStoreImpl.getState().mode).toBe('light');
+  });
+});
