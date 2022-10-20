58bd35f7f354744311418f064ecdece18d36c93e
Solomon Hawk <solomon.hawk@viget.com>
2022-10-20 10:11:41 -0400

    chore: remove unnecessary commitlint type enum config

--- .commitlintrc.cjs
+++ .commitlintrc.cjs
@@ -1,6 +1,3 @@
 module.exports = {
   extends: ['@commitlint/config-conventional'],
-  rules: {
-    "type-enum": [2, "always", ['feat', 'fix', 'docs', 'style', 'refactor', 'test', 'revert', 'build', 'chore', 'ci']]
-  }
 }
