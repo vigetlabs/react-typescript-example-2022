f7b165edc4dd0e877e8af7cd2ff58cc8ea5f9c68
Solomon Hawk <solomon.hawk@viget.com>
2022-10-21 15:40:50 -0400

    test: add playwright-msw
    I had to remove "type": "module" from the
    project's `package.json` in order to get everything
    working properly. So far I haven't noticed any
    problems with that, but TBD.

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,18 +1,16 @@
 import { devices } from '@playwright/test';
 import dotenv from 'dotenv';
+import path from 'path';
 import type { PlaywrightTestConfig } from '@playwright/test';
 
-/**
- * Read environment variables from file.
- * https://github.com/motdotla/dotenv
- */
 dotenv.config({ path: '.env.e2e' });
 
-/**
- * See https://playwright.dev/docs/test-configuration.
- */
+const playwrightDir = (partialPath: string) =>
+  path.resolve(__dirname, './e2e', partialPath);
+
 const config: PlaywrightTestConfig = {
-  testDir: './e2e',
+  testDir: playwrightDir('./specs'),
+  outputDir: playwrightDir('./results'),
   timeout: 30 * 1000,
   expect: {
     timeout: 5000,
@@ -24,13 +22,19 @@ const config: PlaywrightTestConfig = {
   reporter: process.env.CI
     ? 'github'
     : [
-        ['html', { outputFolder: 'e2e/report/' }],
-        ['json', { outputFile: 'e2e/report/results.json' }],
+        [
+          'html',
+          {
+            outputFolder: playwrightDir('./reports'),
+            open: process.env.CI ? 'never' : 'on-failure',
+          },
+        ],
+        ['json', { outputFile: playwrightDir('./reports/results.json') }],
       ],
   use: {
     actionTimeout: 0,
-    trace: 'on-first-retry',
     baseURL: `http://localhost:${process.env.PORT}`,
+    trace: 'retain-on-failure',
   },
   projects: [
     {
@@ -54,10 +58,10 @@ const config: PlaywrightTestConfig = {
       },
     },
   ],
-  outputDir: 'test-results/',
   webServer: {
     command: 'npm run dev',
     port: Number(process.env.PORT),
+    reuseExistingServer: !process.env.CI,
   },
 };
 
