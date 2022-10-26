f755da3d67809e15789ef58e86faafb5f4e6ce97
Solomon Hawk <solomon.hawk@viget.com>
2022-10-26 12:18:27 -0400

    fix: enable mock service worker in production

--- src/config/index.ts
+++ src/config/index.ts
@@ -3,8 +3,7 @@ export const config = {
   apiRequestTimeout: 10000,
   defaultUserEmail: import.meta.env.VITE_DEFAULT_USER_EMAIL,
   defaultUserPassword: import.meta.env.VITE_DEFAULT_USER_PASSWORD,
-  enableMockServer:
-    !!import.meta.env.DEV && import.meta.env.VITE_ENABLE_MOCK_SERVER === 'true',
+  enableMockServer: import.meta.env.VITE_ENABLE_MOCK_SERVER === 'true',
   enableDevtools:
     !import.meta.env.PROD &&
     import.meta.env.VITE_ENABLE_REDUX_DEVTOOLS === 'true',
